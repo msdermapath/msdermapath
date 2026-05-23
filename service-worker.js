b5791dffbc7ca7d1e49452282b53bec7dd86eb38
msdermapath <frankarciniegaq@gmail.com>
2026-05-23 03:09:50 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,94 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('msdermapath').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
-    })
-  );
+const CACHE_NAME = "ms-dermapath-v2026-05-20";
+
+const urlsToCache = [
+
+'./',
+'./index.html',
+'./manifest.json',
+'./icon.png'
+
+];
+
+
+
+self.addEventListener('install', event => {
+
+self.skipWaiting();
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+
+
+self.addEventListener('activate', event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+return self.clients.claim();
+
+});
+
+
+
+self.addEventListener('fetch', event => {
+
+event.respondWith(
+
+fetch(event.request)
+
+.then(response => {
+
+const responseClone = response.clone();
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+cache.put(event.request, responseClone);
+
+});
+
+return response;
+
+})
+
+.catch(() => {
+
+return caches.match(event.request);
+
+})
+
+);
+
 });
